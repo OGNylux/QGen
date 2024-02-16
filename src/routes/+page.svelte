2a707deca6cf70c5ab318abe5ffbff67bb15327e
Sander Hahn <nylux.22@gmail.com>
2024-02-16 22:51:01 +0100

    yuh

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="js">
+    // @ts-ignore
+    let rand;
+    async function getRand() {
+      const response = await fetch("http://localhost:80/rand")
+
+      const random = await response.json();
+      const test = random.number
+      rand = test
+      console.log(rand)
+    }
+
+    let foo = 0
+	  let bar = 1
+	  let result = null
+
+    async function test2() {
+      const res = await fetch('http://localhost:80/test', {
+			method: 'POST',
+			body: JSON.stringify({
+				foo,
+				bar
+			  })
+		  })
+		
+		  const json = await res.json()
+		  result = JSON.stringify(json)
+      
+    }
+  </script>
+  <h1>Your number is {result}!</h1>
+  <button on:click={() => test2()}>Get a random number</button>
---
+++ src/routes/form/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { Home } from "lucide-svelte";
+
+
+</script>
+
+<div class="flex flex-row h-screen">
+    <div class="flex flex-col items-center p-1 w-16 m-1 rounded-2xl bg-slate-200">
+        <button class="flex w-16 h-16 rounded-2xl justify-center items-center pb-1 bg-slate-300 text-stone-800">
+            <Home size={30}/>
+        </button>
+    </div>
+    <p class="text-4xl">This is the form page</p>
+</div>

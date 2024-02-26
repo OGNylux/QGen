4bf00a14fa1e66d1e79c2eeb053ad33add2df455
Sander Hahn <nylux.22@gmail.com>
2024-02-26 12:49:27 +0100

    fixed store bug

--- src/lib/components/multiInput.svelte
+++ src/lib/components/multiInput.svelte
@@ -1,24 +1,48 @@
 <script lang="ts">
     import type { QuestItem } from "$lib/data";
-    import { Plus } from "lucide-svelte";
+    import { MinusCircle, Plus } from "lucide-svelte";
 
     export let quest : QuestItem;
     export let mode : string;
 
-    let arr = mode == "condition" ? quest.tagConditions : quest.tagRewards;
-
     function newDialogue() {
-        arr.push("");
-        arr = [...arr];
+        if(mode === "condition") {quest.tagConditions.push(""); quest.tagConditions = [...quest.tagConditions]}
+        else {quest.tagRewards.push(""); quest.tagRewards = [...quest.tagRewards]}
+    }
+
+    function removeFromArray(arr: string[], index: number) {
+        if(arr.length > 1) arr.splice(index, 1);
+        else arr[0] = "";
     }
 </script>
 
 <div class="flex flex-col w-full h-36 place-items-center overflow-y-auto rounded-xl border-2 border-neutral-700 p-2 shadow-popover">
-    {#each arr as string, i}
-        <input placeholder="Enter the {i+1}. Tag"
-            class="block bg-neutral-800 w-full rounded-md py-1.5 pl-3 pr-3 mb-1 focus:outline-none focus:ring-2 focus:ring-emerald-600" 
-            type="text" bind:value={string} />
-    {/each}
+    {#if mode === "condition"}
+        {#each quest.tagConditions as _, i}
+            <div class="w-full z-10 relative group">
+                <input placeholder="Enter the {i+1}. Tag"
+                    class="block bg-neutral-800 w-full rounded-md py-1.5 pl-3 pr-3 mb-1 focus:outline-none focus:ring-2 focus:ring-emerald-600" 
+                    type="text" bind:value={quest.tagConditions[i]} /> 
+                <button on:click={() => {removeFromArray(quest.tagConditions, i); quest.tagConditions = [...quest.tagConditions]}}
+                    class="absolute -right-1 -top-1 pb-4 opacity-0 group-hover:opacity-100 add-button-hover text-red-700">
+                    <MinusCircle size={20} strokeWidth={3} />
+                </button>
+            </div>
+        {/each}
+    {:else}
+        {#each quest.tagRewards as _, i}
+            <div class="w-full z-10 relative group">
+                <input placeholder="Enter the {i+1}. Tag"
+                    class="block bg-neutral-800 w-full rounded-md py-1.5 pl-3 pr-3 mb-1 focus:outline-none focus:ring-2 focus:ring-emerald-600" 
+                    type="text" bind:value={quest.tagRewards[i]} /> 
+                <button on:click={() => {removeFromArray(quest.tagRewards, i); quest.tagRewards = [...quest.tagRewards]}}
+                    class="absolute -right-1 -top-1 pb-4 opacity-0 group-hover:opacity-100 add-button-hover text-red-700">
+                    <MinusCircle size={20} strokeWidth={3} />
+                </button>
+            </div>
+        {/each}
+    {/if}
+    
     <button on:click={() => newDialogue()} 
         class="flex rounded-lg justify-self-center justify-center items-center w-24 p-0.5 mt-1 opacity-20 hover:opacity-100 add-button-hover
         bg-neutral-800 border-emerald-600 border-2 shadow-md shadow-emerald-600/50 hover:bg-neutral-700/50">
--- src/lib/components/multiTextarea.svelte
+++ src/lib/components/multiTextarea.svelte
@@ -1,27 +1,62 @@
 <script lang="ts">
     import type { QuestItem } from "$lib/data";
-    import { Plus } from "lucide-svelte";
+    import { MinusCircle, Plus } from "lucide-svelte";
 
     export let quest : QuestItem;
     export let mode : string;
 
-    let arr : string[] = mode === "start" ? quest.dialogue : mode === "progress" ? quest.progressDialogue : quest.finishedDialogue;
     function newDialogue() {
-        arr.push("");
-        arr = [...arr]; 
+        if(mode === "start") {quest.dialogue.push(""); quest.dialogue = [...quest.dialogue]}
+        else if(mode === "progress") {quest.progressDialogue.push(""); quest.progressDialogue = [...quest.progressDialogue]}
+        else {quest.finishedDialogue.push(""); quest.finishedDialogue = [...quest.finishedDialogue]}
+    }
+
+    function removeFromArray(arr: string[], index: number) {
+        if(arr.length > 1) arr.splice(index, 1);
+        else arr[0] = "";
     }
 </script>
 
 
 
 <div class="grid w-full h-40 pr-3 place-items-center overflow-y-auto rounded-xl border-2 border-neutral-700 p-2 shadow-popover">
-    {#each arr as tt, i}
-        <div class="w-full">
-            <textarea placeholder="Enter the {tt}. Dialogue" 
-                class="block bg-neutral-800 w-full h-24 rounded-md py-1.5 pl-3 pr-3 mb-2 resize-none focus:outline-none focus:ring-2 focus:ring-emerald-600" 
-                bind:value={arr[i]} on:input={()=> console.log(quest.dialogue)} />
-        </div>
-    {/each}
+    {#if mode === "start"}
+        {#each quest.dialogue as _, i}
+            <div class="w-full z-10 relative group">
+                <textarea placeholder="Enter the {i+1}. Dialogue" 
+                    class="block bg-neutral-800 w-full h-24 rounded-md py-1.5 pl-3 pr-3 mb-2 resize-none focus:outline-none focus:ring-2 focus:ring-emerald-600" 
+                    bind:value={quest.dialogue[i]} />   
+                <button on:click={() => {removeFromArray(quest.dialogue, i); quest.dialogue = [...quest.dialogue]}}
+                    class="absolute -right-1 -top-1 pb-4 opacity-0 group-hover:opacity-100 add-button-hover text-red-700">
+                    <MinusCircle size={20} strokeWidth={3} />
+                </button>
+            </div>
+        {/each}
+    {:else if mode === "progress"}
+        {#each quest.progressDialogue as _, i}
+            <div class="w-full z-10 relative group">
+                <textarea placeholder="Enter the {i+1}. Dialogue" 
+                    class="block bg-neutral-800 w-full h-24 rounded-md py-1.5 pl-3 pr-3 mb-2 resize-none focus:outline-none focus:ring-2 focus:ring-emerald-600" 
+                    bind:value={quest.progressDialogue[i]} />   
+                <button on:click={() => {removeFromArray(quest.progressDialogue, i); quest.progressDialogue = [...quest.progressDialogue]}}
+                    class="absolute -right-1 -top-1 pb-4 opacity-0 group-hover:opacity-100 add-button-hover text-red-700">
+                    <MinusCircle size={20} strokeWidth={3} />
+                </button>
+            </div>
+        {/each}
+    {:else}
+        {#each quest.finishedDialogue as _, i}
+            <div class="w-full z-10 relative group">
+                <textarea placeholder="Enter the {i+1}. Dialogue" 
+                    class="block bg-neutral-800 w-full h-24 rounded-md py-1.5 pl-3 pr-3 mb-2 resize-none focus:outline-none focus:ring-2 focus:ring-emerald-600" 
+                    bind:value={quest.finishedDialogue[i]} />   
+                <button on:click={() => {removeFromArray(quest.finishedDialogue, i); quest.finishedDialogue = [...quest.finishedDialogue]}}
+                    class="absolute -right-1 -top-1 pb-4 opacity-0 group-hover:opacity-100 add-button-hover text-red-700">
+                    <MinusCircle size={20} strokeWidth={3} />
+                </button>
+            </div>
+        {/each}
+    {/if}
     <button on:click={() => newDialogue()} 
         class="flex rounded-lg justify-self-center justify-center items-center w-24 p-0.5 mx-1 opacity-20 hover:opacity-100 add-button-hover
         bg-neutral-800 border-emerald-600 border-2 shadow-md shadow-emerald-600/50 hover:bg-neutral-700/50">
